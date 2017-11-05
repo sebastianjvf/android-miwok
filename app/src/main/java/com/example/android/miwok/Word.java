8bdb0a0e18f518acc421b2bd06b1c7492607a8a1
sebastianjvf <sjvf@Sebastians-MacBook-Pro.local>
2017-11-05 17:55:05 +0100

    feat: Added images and a new constructor to attach images to words.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,6 +4,7 @@ public class Word {
 
     String miwokTranslation;
     String defaultTranslation;
+    int resourceId;
 
     /**
      * Creates a word object encapsulating a miwok word and its translation. Use a string resource to
@@ -15,6 +16,20 @@ public class Word {
     public Word(String miwokTranslation, String defaultTranslation) {
         this.miwokTranslation = miwokTranslation;
         this.defaultTranslation = defaultTranslation;
+        this.resourceId = -1;
+    }
+
+    /**
+     * Creates a word object encapsulating a miwok word, its translation and an ImageResourceID. Use a string resource to
+     * allow for easy translation.
+     *
+     * @param miwokTranslation
+     * @param defaultTranslation
+     */
+    public Word(String miwokTranslation, String defaultTranslation, int resourceId) {
+        this.miwokTranslation = miwokTranslation;
+        this.defaultTranslation = defaultTranslation;
+        this.resourceId = resourceId;
     }
 
     public String getMiwokTranslation() {
@@ -23,6 +38,10 @@ public class Word {
 
     public String getDefaultTranslation() {
         return defaultTranslation;
+    }
+
+    public int getResourceId() {
+        return resourceId;
     }
 
 }
