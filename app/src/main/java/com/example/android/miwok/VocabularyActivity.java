e1e098bab83077359e9310a94dba551ab2f960bf
sebastianjvf <sjvf@n154-p118.eduroam.kth.se>
2017-11-06 18:49:26 +0100

    fix: Sound resources were not played anymore.
    Initialised the AudioManager properly.

--- app/src/main/java/com/example/android/miwok/VocabularyActivity.java
+++ app/src/main/java/com/example/android/miwok/VocabularyActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 public class VocabularyActivity extends AppCompatActivity {
 
@@ -19,29 +21,30 @@ public class VocabularyActivity extends AppCompatActivity {
     };
 
     protected AudioManager audioManager;
+
     protected AudioManager.OnAudioFocusChangeListener mAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
         @Override
         public void onAudioFocusChange(int focusChange) {
             if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
                 mediaPlayer.pause();
                 mediaPlayer.seekTo(0);
-                Log.v("1111111111", "fired #1");
             } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
                 // Permanently lost focus; release MediaPlayer and stop playing.
                 releaseMediaPlayer();
-                Log.v("1111111111", "fired #2");
             } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
                 mediaPlayer.pause();
                 mediaPlayer.seekTo(0);
-                Log.v("1111111111", "fired #3");
             } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
                 mediaPlayer.start();
-                Log.v("1111111111", "fired #4");
             }
-
-            Log.v("1111111111", "fired #3");
         }
     };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+    }
 
     /**
      * Releases the media resource when the app is stopped
