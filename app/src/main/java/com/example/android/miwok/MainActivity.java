1b61e073427fd26793fb13560f379c6bba271b11
sebastianjvf <sjvf@Sebastians-MacBook-Pro.local>
2017-11-07 19:46:27 +0100

    feat: Added tabs to switch between fragments.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -30,9 +31,14 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
+        // Find the view pager which will display a fragment
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
 
+        // Create an adapter that knows which fragment should be shown on each page
         FragmentPagerAdapter pagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+
+            private String tabTitles[] = new String[]{"Numbers", "Colours", "Family", "Phrases"};
+
             @Override
             public int getCount() {
                 return 4;
@@ -53,9 +59,24 @@ public class MainActivity extends AppCompatActivity {
                         return new NumbersFragment();
                 }
             }
+
+            /**
+             * Generate a tab title based on item position
+             * @param position
+             * @return
+             */
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return tabTitles[position];
+            }
         };
 
+        // Attach the Adapter to the ViewPager
         viewPager.setAdapter(pagerAdapter);
+
+        // Find the Tab Layout and attach the ViewPager as well
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 
